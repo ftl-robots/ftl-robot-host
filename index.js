f77c209b108ba512542e76e5061635965c7edfa2
Zhiquan Yeo <zyeo8@bloomberg.net>
2017-08-24 14:18:10 -0400

    Expose Serial Interface

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const Robot = require('./robot');
 const Constants = require('./constants');
 const I2CInterface = require('./interfaces/i2c-iface');
 const DeviceInterface = require('./devices/ftl-device-iface');
+const SerialInterface = require('./interfaces/serial-iface');
 
 module.exports = {
     Robot: Robot,
     Constants: Constants,
     Interfaces: {
-        I2C: I2CInterface
+        I2C: I2CInterface,
+        SERIAL: SerialInterface
     },
     DeviceInterface: DeviceInterface
 };
